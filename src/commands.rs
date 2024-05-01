85454be56ca563406863b24debfb337f791d4a40
Myles Berueda <myles@polypixel.io>
2024-05-01 01:50:42 -0700

    fix: removes extra commands from commands.rs

--- src/commands.rs
+++ src/commands.rs
@@ -1,8 +1,3 @@
 pub(crate) mod example;
 #[cfg(debug_assertions)]
 pub(crate) mod scaffold;
-pub(crate) mod some_command;
-pub(crate) mod delete;
-pub(crate) mod delete_1;
-pub(crate) mod delete_2;
-pub(crate) mod delete_3;
